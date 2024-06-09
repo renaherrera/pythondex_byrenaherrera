ce3ba1d5f06ae3ebc580f79ff7056d4df2addfbd
renaherrera <re.herrerac@duocuc.cl>
2024-06-09 18:47:18 -0400

    Pythondex v1.0 BETA by Renato Herrera

--- functions.py
+++ functions.py
@@ -1,10 +1,40 @@
 import json
+import msvcrt
+import os
+
+def error(p_message):
+    os.system("cls")
+    print(p_message)
+    msvcrt.getch()
+    os.system("cls")
+def pres_tecla(p_message):
+    print(p_message)
+    msvcrt.getch()
+    os.system("cls")
+
+def menu(options):
+    while True:
+        print("Pythondex | Renato Herrera")
+        print("-"*50)
+        print("1. Buscar pokémon")
+        print("2. Salir")
+        print("-"*50)
+        try:
+            opc = int(input("Selecciona la opción que deseas: "))
+            if opc in options:
+                break
+            else:
+                error("ERROR! opción incorrecta!")
+        except:
+            error("ERROR! debe ser entero!")
+    return opc
 
 def buscar_pokemon(p_pokemons, p_pokemon):
     with open(p_pokemons, "r", encoding='utf-8') as archivo:
         lector = json.load(archivo)
         for i in range(len(lector)):
             if p_pokemon.capitalize() == lector[i]["name"]["english"]:
+                print("")
                 print("Pokemon:")
                 print(f"\tID: {lector[i]["id"]}")
                 print(f"\tNombre: {lector[i]["name"]["english"]}")
@@ -19,3 +49,7 @@ def buscar_pokemon(p_pokemons, p_pokemon):
                 print(f"\tSp. Attack: {lector[i]["base"]["Sp. Attack"]}")
                 print(f"\tSp. Defense: {lector[i]["base"]["Sp. Defense"]}")
                 print(f"\tSpeed: {lector[i]["base"]["Speed"]}")
+                print("")
+                pres_tecla("Presiona cualquier tecla para continuar...")
+                return
+        error("ERROR! pokémon no encontrado!")
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import json
 import os
 from functions import *
 
@@ -7,17 +6,12 @@ os.system("cls")
 pokemons = "pokedex.json"
 
 while True:
-    print("Pythondex | Renato Herrera")
-    print("-"*50)
-    print("1. Buscar pokémon")
-    print("2. Salir")
-    print("-"*50)
-    opc = int(input("Selecciona la opción que deseas: "))
+    opc = menu( [1,2] )
 
     os.system("cls")
     if opc == 1:
         pokemon = input("Ingresa el pokemon que quieres buscar: ")
-
         buscar_pokemon(pokemons, pokemon)
     else:
+        print("Adiós, nos vemos pronto!")
         break
