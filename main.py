12c9b75de25260400b7ebf2871704ff3c4002394
renaherrera <re.herrerac@duocuc.cl>
2024-06-09 17:44:11 -0400

    Menú inicial listo

---
+++ main.py
@@ -0,0 +1,19 @@
+import json
+import os
+
+pokemons = "pokemon.json"
+
+while True:
+    print("Pythondex | Renato Herrera")
+    print("-"*50)
+    print("1. Buscar pokémon"*50)
+    print("2. Salir")
+    print("-"*50)
+    opc = int(input("Selecciona la opción que deseas: "))
+
+    if opc == 1:
+        pass
+    elif opc == 2:
+        pass
+    else:
+        break
